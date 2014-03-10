564fc212ff002f5f98c2aca6febf01a2884d261d
Joshua Tanner <biolsurfer85@gmail.com>
2014-03-10 05:54:34 -0700

    Add about page

--- public/www/js/app.js
+++ public/www/js/app.js
@@ -13,5 +13,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'DashCtrl'});
   $routeProvider.when('/home', {templateUrl: 'partials/home.html'});
   $routeProvider.when('/help', {templateUrl: 'partials/help.html'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
